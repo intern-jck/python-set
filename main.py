4dbbb72446f0b8c3deb7ab63d758a7fe7cc5c5d9
makerinchief <info@makerinchief.com>
2021-10-05 10:31:33 -0400

    Newest Version Upload
    Forgot to push the changes I made on my laptop to the source.

--- main.py
+++ main.py
@@ -21,19 +21,30 @@ draw_three_button = tk.Button(
 check_set_button = tk.Button(
     table, text="Check Set", font=button_font, command=game.check_set
 )
+# check_button = tk.Button(
+#     table, text="Check Set", font=button_font, command=game.button_check
+# )
 
-cards_left = tk.StringVar()
-cards_left.set(game.cards_left())
-
-cards_left_label = tk.Label(table, textvariable=cards_left, relief="raised")
-
-cards_left_label.place(x=600, y=50)
+easy_game_button = tk.Button(
+    table, text="Easy Game", font=button_font, command=game.easy_game
+)
 
 new_game_button.place(x=700, y=100)
 draw_three_button.place(x=700, y=150)
 check_set_button.place(x=700, y=200)
+easy_game_button.place(x=700, y=400)
+
+
+# Scoreboard
+cards_left_label = tk.Label(table, text="Cards Left", font=button_font, relief="raised")
+cards_left_label.place(x=600, y=50)
+
+
+def update_score():
+    cards_left_label.config(text="Cards Left:" + game.cards_left())
+    table.after(250, update_score)
+
+
+table.after(250, update_score)
 
 table.mainloop()
-
-# while True:
-#     cards_left.set(str(game.cards_left()))
--- set_game.py
+++ set_game.py
@@ -10,43 +10,36 @@ import glob
 
 class Set_Game:
 
-    # Tkinter setup
-    # The tk window will be our "table" for the card card
-    # table = tk.Tk()
-    # table.title("Set Game")
-    # table.geometry("800x500")
-    # table.config(bg="grey")
-
     # Number of cards in deck
     MAX_CARDS = 81
-    # Keep track of th cards left in the deck.  When deck = 0, game is over
-    card_deck = [i for i in range(MAX_CARDS)]
-    # Value for each card, [0,0,0,0] ==> [Red, 1, Empty, Triangle]
+
+    # Value for each card, [0,0,0,0] ==> [Red, 1, Empty, Circle]
     CARD_VALUES = [[0, 0, 0, 0] for i in range(MAX_CARDS)]
+
     # Each card is represented by a Tkinter button
     CARD_BUTTONS = [0 for i in range(MAX_CARDS)]
-    card_pos = [[0, 0] for i in range(MAX_CARDS)]
-    # Keep track of cards drawn
-    # not sure if needed
-    # cards_drawn = [False for i in range(MAX_CARDS)]
 
     # Keep track of which cards are selected on table
     card_selected = [False for i in range(MAX_CARDS)]
+
     # Save the selected cards in a list to check if the set of 3 is a Set
     card_set = []
 
     # Keep track of available spots for cards on the table
     # Use this to add new cards when sets are made
     # 1 ==> Spot taken, 0 ==> Spot free
+    CARD_TABLE_ROWS = 3
+    CARD_TABLE_COLS = 5
     card_table = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 
     def __init__(self, master):
 
-        # Use the frame initialize in main.py
+        # Use the frame initialized in main.py
         self.master = master
 
         # Get the images
         self.CARD_IMAGE_PATHS = glob.glob("Setcards\*.png")
+
         # The order of the cards matters
         # The ternary value representing the card is based on its place in line
         self.CARD_IMAGE_PATHS = sorted(
@@ -58,14 +51,16 @@ class Set_Game:
             self.card_images.append(self.card_image)
 
         # In case you want to display the ternary value on the card buttons
-        self.button_font = tkfont.Font(family="System", size=8, weight="bold")
+        self.button_font = tkfont.Font(family="System", size=10, weight="bold")
 
+        # Create each card
         for i in range(self.MAX_CARDS):
 
             # Create a ternary value to represent each card
             self.CARD_VALUES[i] = self.ternary(i)
-
+            # Text to show button card number and value
             button_text = str(i) + ": " + str(self.CARD_VALUES[i])
+
             # Create a button for each card
             card = tk.Button(
                 self.master,
@@ -75,15 +70,12 @@ class Set_Game:
                 bg="black",
                 fg="white",
                 compound="top",
-                # height=5,
-                # width=10,
                 command=partial(self.card_click, i),
             )
             # Add it to the list
             self.CARD_BUTTONS[i] = card
 
-    # Each card can be represented by a 4 digit ternary value
-    # For example, a Red, Empty, Single, Circle is [0,0,0,0]
+    # Function to create a ternary value based on input
     def ternary(self, n):
         if n == 0:
             return [0, 0, 0, 0]
@@ -107,42 +99,66 @@ class Set_Game:
         elif len(num) == 4:
             return num
 
-    def new_game(self):
+    def easy_game(self):
 
-        # Create a deck
         self.card_deck = [i for i in range(self.MAX_CARDS)]
-        # Shuffle the deck
-        random.shuffle(self.card_deck)
+        print(self.card_deck)
 
-        # Lay 12 cards out on the table to form a grid
-        # Orientation now very important, 3 rows by 4 columns fits better though
         for i in range(3):
             for j in range(4):
 
                 # Pick cards from the deck
-                self.card_picked = self.card_deck[0]
-                print(self.card_picked, " ", end="")
-                # Place them into the frame
-                self.CARD_BUTTONS[self.card_picked].grid(
-                    row=i, column=j, padx=10, pady=10
-                )
-                # Keep track of positions
-                self.card_pos[self.card_picked] = i, j
-                # Keep track of available spots on table
-                self.card_table[i][j] = 1
+                new_card = self.draw_card()
 
-                # Remove the picked cards from the deck
-                self.card_deck.pop(0)
+                # Place them into the frame
+                self.CARD_BUTTONS[new_card].grid(row=i, column=j, padx=10, pady=10)
+
+                # Keep track of available spots on table
+                self.card_table[i][j] = new_card
+
+                print(" {}, ".format(self.card_table[i][j]), end=" ")
+
             print()
         print("Cards Left: {}".format(self.cards_left()))
+
+    def new_game(self):
+
+        # Create a deck
+        self.card_deck = [i for i in range(self.MAX_CARDS)]
+        # print(self.card_deck)
+
+        # shuffle the deck
+        random.shuffle(self.card_deck)
+        # print(self.card_deck)
+
+        # Clear out the Set if any left from previous game
+        for i in range(len(self.card_set)):
+            self.card_set.pop(0)
+
+        # Lay 12 cards out on the table to form a grid with 3 rows, 4 columns
+        for i in range(3):
+            for j in range(4):
+
+                # Pick a card
+                new_card = self.draw_card()
+
+                # Place them into the frame
+                self.CARD_BUTTONS[new_card].grid(row=i, column=j, padx=10, pady=10)
+
+                # Keep track of cards on table
+                self.card_table[i][j] = new_card
+
+                print(" {}, ".format(self.card_table[i][j]), end=" ")
+            print()
 
     # Add three more cards to the table
     def draw_three(self):
         for i in range(3):
-            new_card = self.card_deck[0]
+
+            new_card = self.card_deck[self.draw_card()]
+
             self.CARD_BUTTONS[new_card].grid(row=i, column=5)
-            self.card_deck.pop(0)
-            # print("Cards Left: {}".format(self.cards_left()))
+            self.card_table[i][4] = new_card
 
     def cards_left(self):
         return str(len(self.card_deck))
@@ -152,6 +168,7 @@ class Set_Game:
         self.card_selected[num] = not self.card_selected[num]
         # print(self.CARD_VALUES[num])
         # print(self.card_pos[num])
+        # print(self.CARD_BUTTONS[num].grid_info())
 
         if self.card_selected[num] == True:
 
@@ -159,8 +176,11 @@ class Set_Game:
                 self.card_set.append(num)
                 self.CARD_BUTTONS[num].config(bg="green")
                 print(self.card_set)
+                card_info = self.CARD_BUTTONS[num].grid_info()
+                print(card_info["row"], card_info["column"])
 
         elif self.card_selected[num] == False:
+
             if num in self.card_set:
                 self.index = self.card_set.index(num)
                 self.card_set.pop(self.index)
@@ -169,12 +189,14 @@ class Set_Game:
 
     def check_set(self):
 
+        # If there are 3 cards selected
         if len(self.card_set) == 3:
-
+            # Get the ternary values for each card
             card_1 = self.CARD_VALUES[self.card_set[0]]
             card_2 = self.CARD_VALUES[self.card_set[1]]
             card_3 = self.CARD_VALUES[self.card_set[2]]
 
+            # Comapare the three values
             if (
                 (
                     card_1[0] == card_2[0] == card_3[0]
@@ -194,49 +216,62 @@ class Set_Game:
                 )
             ):
                 print("Set Found!")
+                # Get the grid positions of the three cards
+                card_1_info = self.CARD_BUTTONS[self.card_set[0]].grid_info()
+                card_2_info = self.CARD_BUTTONS[self.card_set[1]].grid_info()
+                card_3_info = self.CARD_BUTTONS[self.card_set[2]].grid_info()
+
                 # Take these cards off the table
                 self.CARD_BUTTONS[self.card_set[0]].grid_forget()
                 self.CARD_BUTTONS[self.card_set[1]].grid_forget()
                 self.CARD_BUTTONS[self.card_set[2]].grid_forget()
-                # Get the grid location of these cards
-                card_1_x, card_1_y = self.card_pos[self.card_set[0]]
-                card_2_x, card_2_y = self.card_pos[self.card_set[1]]
-                card_3_x, card_3_y = self.card_pos[self.card_set[2]]
 
-                # Place a card at the first location
-                self.CARD_BUTTONS[self.card_deck[0]].grid(
-                    row=card_1_x,
-                    column=card_1_y,
+                # Place three new cards on the table
+                new_card = self.draw_card()
+                self.CARD_BUTTONS[new_card].grid(
+                    row=card_1_info["row"],
+                    column=card_1_info["column"],
                     padx=10,
                     pady=10,
                 )
-                # Remove this card from the deck
-                self.card_deck.pop(0)
 
                 # Repeat 2 more times
-                self.CARD_BUTTONS[self.card_deck[0]].grid(
-                    row=card_2_x,
-                    column=card_2_y,
+                new_card = self.draw_card()
+                self.CARD_BUTTONS[new_card].grid(
+                    row=card_2_info["row"],
+                    column=card_2_info["column"],
                     padx=10,
                     pady=10,
                 )
-                self.card_deck.pop(0)
 
-                self.CARD_BUTTONS[self.card_deck[0]].grid(
-                    row=card_3_x,
-                    column=card_3_y,
+                new_card = self.draw_card()
+                self.CARD_BUTTONS[new_card].grid(
+                    row=card_3_info["row"],
+                    column=card_3_info["column"],
                     padx=10,
                     pady=10,
                 )
-                self.card_deck.pop(0)
 
-                # Clear out the card_set[]
-                self.card_set.pop(0)
-                self.card_set.pop(0)
-                self.card_set.pop(0)
+                # Clear out the Set if any left from previous game
+                for i in range(len(self.card_set)):
+                    self.card_set.pop(0)
 
             else:
                 print("Not A Set!")
 
         else:
             print("Need A Full Set!")
+
+    # def sort_table(self):
+    #     current_table = []
+    #     for i in self.CARD_TABLE_ROWS:
+    #         for j in self.CARD_TABLE_COLS:
+    #             if self.card_table[i][j] == 1:
+    #                 cuurent_table.append(self)
+
+    def draw_card(self):
+        # Draw a card from the deck
+        new_card = self.card_deck[0]
+        # Remove this card from the deck
+        self.card_deck.pop(0)
+        return new_card
